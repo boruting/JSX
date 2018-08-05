ecd8c0821c06c182dc7bd489263039f56173756a
boruting <41093942+boruting@users.noreply.github.com>
2018-08-05 13:12:31 +0800

    Delete 连接对象.jsx

--- 重命名/连接对象.jsx
+++
@@ -1,9 +0,0 @@
-var pat = "C:\\Users\\Administrator\\Desktop\\test\\";
-var name1 = "bc.psd";
-var idPlc = charIDToTypeID( "Plc " );
-var desc634 = new ActionDescriptor();
-var idnull = charIDToTypeID( "null" );
-desc634.putPath( idnull, new File( pat+name1));//这里应该是路径加获取到到名字
-var idLnkd = charIDToTypeID( "Lnkd" );//定义为连接对象
-desc634.putBoolean( idLnkd, true );
-executeAction( idPlc, desc634, DialogModes.NO );
