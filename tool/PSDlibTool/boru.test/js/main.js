d1f199a3adfb462eee4094d98de541a1dad5bb06
boruting <boruting@naver.com>
2018-09-15 01:38:57 +0800

    add 测试版  cep

---
+++ tool/PSDlibTool/boru.test/js/main.js
@@ -0,0 +1,54 @@
+var test = function () {
+    cs = new CSInterface();
+    cs.evalScript("testJsx()",function(result){
+        var o = result ;
+        alert(o);
+    })
+
+   
+    
+}
+
+/**
+ * 初始化
+ * 
+ */
+window.onload = function () {
+    cs = new CSInterface();
+    //var arrayObj =  cs.evalScript("testJsx()")
+	var arrayObj = arr();
+
+	listBtn(arrayObj);
+
+}
+/**
+ * 生产PSD 按钮列表
+ * @param {*} arrayObj psd 路径和名字的数组
+ */
+var listBtn = function (arrayObj) {
+
+	var td1 = new Array();
+
+	for (var i = 0; i < arrayObj.length; i++) {
+
+		td1[i] = document.createElement("td");
+		td1[i].innerHTML = '<input type="button" value="' + arrayObj[i] + '" onclick="addPSD(this)" />';
+
+		document.body.appendChild(td1[i]);
+
+	}
+}
+/**
+ * 获取 PSD 文件 路径和名字
+ */
+var arr = function () {
+	var arrayObj = ["aaaaaa", "bbbbb", "cccc", "dddd", "eeee"];
+	return arrayObj;
+}
+/**
+ * 添加连接对象到 对当前PSD中
+ */
+function addPSD() {
+
+	alert("添加psd");
+}
---
+++ tool/PSDlibTool/boru.test/jsx/main.jsx
@@ -0,0 +1,28 @@
+﻿var path = "E:/workspace/test/PSD";
+var testJsx = function (path){
+    //var path = "E:/workspace/test/PSD";
+    var psdFolder = Folder (path);
+    var psdFolderList = psdFolder.getFiles();
+        
+    //var 
+
+    //声明一个新数组
+    var arr =new Array();
+    //遍历所有文件  获取到文件到名字
+    for (var i = 0; i<psdFolderList.length;i++){
+               var fileName = decodeURI(psdFolderList[i].name);
+               //存放到新数组里
+             arr[i] = fileName
+            
+        }
+     $.writeln(arr );
+    return arr;
+    //返回这个数组
+   
+   // var a = decodeURI(psdFolderList);
+    //return psdFolderList;
+    //return a;
+    //$.writeln(a );
+}
+//testJsx(path);
+
