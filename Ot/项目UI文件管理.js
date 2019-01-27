662a70b96572ba7db46f06f6e288efb0f3c38aa9
boruting <41093942+boruting@users.noreply.github.com>
2019-01-27 20:21:34 +0800

    Update 项目UI文件管理.js

--- Ot/项目UI文件管理.js
+++ Ot/项目UI文件管理.js
@@ -4,27 +4,14 @@ Company.projectName.PSD = {};
 Company.projectName.UI = {};
 //切图文件路径 
 /**
- * 例如：主界面(psd)
- * PSD目录：fskj/wwjx/psd/主界面/主角头像信息.psd
- * UI目录： fskj/wwjx/ui/主界面/主角头像信息/示意图_主角头像信息.png 
- * 
+ * 公司名称
+ * 项目名称
+ * 文件夹类型 [psd,ui]
  * 
  */
-var test = "这是个测试";
 
-/**
- * 切图插件 
- * 获取psd 和 UI 路径 
- * 保证 psd 文件夹 和 UI 文件夹 路径结构统一
- * 切图方式参考 psd到unity 脚本
- */
-
-var fullPath = "fskj/wwjx/psd/主界面/主角头像信息.psd";//var fullPath = doc.fullName;
-var psdPath = fullPath.substr(0, pat.length - 5);
-var pasPathArr = psdPath.split("/");
-var lower = pasPathArr[2].toLowerCase();
-if (lower == "psd") {
-    pasPathArr.splice(2, 1, "ui");
+testCompany = {
+    name: 'fskj',
+    project:'yyd'
 }
-var uiPath = pasPathArr.join("/");
-console.log(uiPath); 
+console.log(testCompany.name);
